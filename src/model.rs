2093a6e5f8a52b25ec1329927350aa699edd9eed
Oskár Adam Válent <xvalent@fi.muni.cz>
2023-07-30 18:42:25 +0200

    comments

--- src/model.rs
+++ src/model.rs
@@ -8,12 +8,16 @@ use std::ops::Deref;
 pub struct SbmlModel(XmlElement);
 
 /// Adds the default implementation of [SBase] to the [SbmlModel].
+/// Allows to get/set id, name, etc. of [SbmlModel]
 impl SBaseDefault for SbmlModel {}
 
+/// Public functions to manipulate with the contents of [SbmlModel]
+/// i.e., optional lists inside SBML model + constructor new() 
 impl SbmlModel {
     pub fn new(xml: XmlElement) -> SbmlModel {
         SbmlModel::from(xml)
     }
+
     pub fn get_function_definitions(&self) -> XmlList<SbmlFunctionDefinition> {
         let list_element = {
             let xml = self.read_doc();
@@ -25,6 +29,7 @@ impl SbmlModel {
     }
 
     pub fn get_unit_definitions(&self) -> XmlList<SbmlUnitDefinition> {
+        // possible better-readability & no copy-paste approach
         let list = self.child_element("listOfUnitDefinitions");
         XmlList::from(self.as_xml().derive(list))
     }
