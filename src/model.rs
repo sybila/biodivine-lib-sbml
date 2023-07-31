9364fba37975f9752ecd5d883d15214a05049233
Oskár Adam Válent <xvalent@fi.muni.cz>
2023-07-31 20:27:16 +0200

    comments

--- src/model.rs
+++ src/model.rs
@@ -297,6 +297,7 @@ impl From<XmlElement> for Compartment {
 impl SBaseDefault for Compartment {}
 
 impl Compartment {
+    /// override default implementation as compartment id is required
     pub fn get_id(&self) -> String {
         let doc = self.read_doc();
         self.element()
@@ -305,6 +306,7 @@ impl Compartment {
             .unwrap()
     }
 
+    /// return String or Double ?
     pub fn get_spatial_dimensions(&self) -> Option<String> {
         let doc = self.read_doc();
         self.element()
@@ -312,6 +314,7 @@ impl Compartment {
             .map(|it| it.to_string())
     }
 
+    /// return String or Double
     pub fn get_size(&self) -> Option<String> {
         let doc = self.read_doc();
         self.element()
@@ -319,6 +322,7 @@ impl Compartment {
             .map(|it| it.to_string())
     }
 
+    /// TODO: implement units lookup in model according to documentation
     pub fn get_units(&self) -> Option<String> {
         let doc = self.read_doc();
         self.element()
@@ -326,6 +330,7 @@ impl Compartment {
             .map(|it| it.to_string())
     }
 
+    /// return String or Boolean ?
     pub fn get_constant(&self) -> Option<String> {
         let doc = self.read_doc();
         self.element()
