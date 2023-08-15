1aeb7c4430c288efd02cd46e994c624a01b2ec3f
adamValent <adam.valent88@gmail.com>
2023-08-15 13:01:42 +0200

    add conversion from/to xml attribute
    - f64
    - Option<f64>
    - BaseUnit
    - Option<BaseUnit>

--- src/xml/impl_xml_property_type.rs
+++ src/xml/impl_xml_property_type.rs
@@ -3,7 +3,9 @@
 //      section 3.1.1). I believe these should be handled by `xml-doc` already, but we should
 //      have a test case for this.
 
-use crate::xml::XmlPropertyType;
+use std::str::FromStr;
+
+use crate::{model::BaseUnit, xml::XmlPropertyType};
 
 /// A "trivial" conversion between an XML attribute and a `String`. When the attribute value
 /// is missing, returns a "Value missing." error.
@@ -88,5 +90,84 @@ impl XmlPropertyType for i32 {
 
     fn write(&self) -> Option<String> {
         Some(format!("{}", self))
+    }
+}
+
+/// A "trivial" conversion between an XML attribute and a `f64` floating-point number (`double` 
+/// type in the SBML specification). Missing attribute value is interpreted as an error.
+///
+/// ## Specification
+///  - Section 3.1.5
+impl XmlPropertyType for f64 {
+    fn try_read(value: Option<&str>) -> Result<Self, String> {
+        match value {
+            Some(value) => match value.parse::<f64>() {
+                Ok(x) => Ok(x),
+                Err(e) => Err(format!(
+                    "Value `{value}` does not represent a valid signed integer ({}).",
+                    e
+                )),
+            },
+            None => Err("Value missing".to_string()),
+        }
+    }
+
+    fn write(&self) -> Option<String> {
+        Some(format!("{}", self))
+    }
+}
+
+/// A "trivial" conversion between an XML attribute and a `f64` floating-point number (`double`
+/// type in the SBML specification).
+///
+/// ## Specification
+///  - Section 3.1.5
+impl XmlPropertyType for Option<f64> {
+    fn try_read(value: Option<&str>) -> Result<Self, String> {
+        match value {
+            Some(_) => Ok(Some(f64::try_read(value).unwrap())),
+            None => Ok(None),
+        }
+    }
+
+    fn write(&self) -> Option<String> {
+        Some(format!("{:#?}", self))
+    }
+}
+
+/// A conversion between an XML attribute and a [BaseUnit] value. Missing attribute value is 
+/// interpreted as an error. 
+/// 
+/// ## Specification
+///  - Section 4.4.2
+impl XmlPropertyType for BaseUnit {
+    fn try_read(value: Option<&str>) -> Result<Self, String> {
+        match value {
+            Some(value) => match BaseUnit::from_str(value) {
+                Ok(unit) => Ok(unit),
+                Err(e) => Err(format!(
+                    "Value `{value}` does not represent a valid base unit ({})",
+                    e
+                )),
+            },
+            None => Err("Value missing".to_string()),
+        }
+    }
+
+    fn write(&self) -> Option<String> {
+        Some(format!("{}", self))
+    }
+}
+
+impl XmlPropertyType for Option<BaseUnit> {
+    fn try_read(value: Option<&str>) -> Result<Self, String> {
+        match value {
+            Some(_) => Ok(Some(BaseUnit::try_read(value).unwrap())),
+            None => Ok(None)
+        }
+    }
+
+    fn write(&self) -> Option<String> {
+        Some(format!("{:#?}", self))
     }
 }
