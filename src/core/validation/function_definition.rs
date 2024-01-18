141f3ea2d511a0045102b54dbfc34d2e329c7ee2
adamValent <adam.valent88@gmail.com>
2024-01-18 16:51:06 +0100

    Add validation skeleton

---
+++ src/core/validation/function_definition.rs
@@ -0,0 +1,8 @@
+use crate::core::FunctionDefinition;
+use crate::SbmlIssue;
+
+impl FunctionDefinition {
+    pub(crate) fn validate(&self, _issues: &mut Vec<SbmlIssue>) {
+        // todo!()
+    }
+}
