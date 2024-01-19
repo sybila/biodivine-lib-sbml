b5439d6b58ef8b2bffe520256ac7dd11e4d9c0db
adamValent <adam.valent88@gmail.com>
2024-01-19 16:01:27 +0100

    Fix typo

--- src/core/validation/function_definition.rs
+++ src/core/validation/function_definition.rs
@@ -4,7 +4,7 @@ use crate::xml::XmlWrapper;
 use crate::SbmlIssue;
 
 impl FunctionDefinition {
-    pub(crate) fn validate(&self, _issues: &mut Vec<SbmlIssue>) {
-        apply_rule_10102(self.xml_element(), _issues);
+    pub(crate) fn validate(&self, issues: &mut Vec<SbmlIssue>) {
+        apply_rule_10102(self.xml_element(), issues);
     }
 }
