ffd69092b495988bb210d28043fe0622f6aadd89
Kateřina Bosáková <xbosakov@fi.muni.cz>
2025-04-04 12:56:20 +0200

    Verifications for layout elements.

---
+++ src/layout/validation/bounding_box.rs
@@ -0,0 +1,24 @@
+use std::collections::HashSet;
+use crate::core::{MetaId, SId};
+use crate::core::validation::sbase::validate_sbase;
+use crate::core::validation::SbmlValidable;
+use crate::core::validation::type_check::CanTypeCheck;
+use crate::layout::bounding_box::BoundingBox;
+use crate::SbmlIssue;
+use crate::xml::{RequiredXmlChild, XmlProperty};
+
+impl SbmlValidable for BoundingBox{
+    fn validate(&self, issues: &mut Vec<SbmlIssue>, identifiers: &mut HashSet<SId>, meta_ids: &mut HashSet<MetaId>) {
+        validate_sbase(self, issues, identifiers, meta_ids);
+        
+        self.position().get().validate(issues, identifiers, meta_ids);
+        self.dimensions().get().validate(issues, identifiers, meta_ids);
+        
+        if self.position().get().z().is_set() && !self.dimensions().get().depth().is_set(){
+            let message = "If [z] attribute of [position] is not specified the [depth] attribute of [dimensions] must also not be specified!";
+            issues.push(SbmlIssue::new_error("21305", self, message))
+        }
+    }
+}
+
+impl CanTypeCheck for BoundingBox{}
---
+++ src/layout/validation/dimensions.rs
@@ -0,0 +1,15 @@
+use std::collections::HashSet;
+use crate::core::{MetaId, SId};
+use crate::core::validation::sbase::validate_sbase;
+use crate::core::validation::SbmlValidable;
+use crate::core::validation::type_check::CanTypeCheck;
+use crate::layout::dimensions::Dimensions;
+use crate::SbmlIssue;
+
+impl SbmlValidable for Dimensions {
+    fn validate(&self, issues: &mut Vec<SbmlIssue>, identifiers: &mut HashSet<SId>, meta_ids: &mut HashSet<MetaId>) {
+        validate_sbase(self, issues, identifiers, meta_ids);
+    }
+}
+
+impl CanTypeCheck for Dimensions{}
